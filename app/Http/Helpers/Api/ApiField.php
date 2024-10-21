d9860b0170d534604bfd58ad83c293283342f7e6
Mohammad Ezz <mohammad.ezzeldin.hassan@outlook.com>
2024-10-21 21:54:48 +0400

    Refactor ApiField file and add aliasFieldsName function

--- app/Http/Helpers/Api/ApiField.php
+++ app/Http/Helpers/Api/ApiField.php
@@ -12,12 +12,22 @@ class ApiField {
             $selectedFields= [];
             foreach($fields as $field_key=>$field_value) {
                 if(in_array($field_key, $urlFields)) {
-                    $selectedFields[$field_key] = $field_value;
+                    $selectedFields[] = $field_value . " as " . $field_key;
                 }
             }
             return $selectedFields;
         }
     }
+
+    public function aliasFieldsName($fields, array $cutomeAlies = []) {
+        $selectedFields = [];
+        $fields = array_merge($fields, $cutomeAlies);
+        foreach($fields as $field_key=>$field_value ) {
+            $selectedFields[] = $field_value . " as " . $field_key;
+        }
+        return $selectedFields;
+    }
+
     private function checkFields($urlFields, $fields) {
             $urlFields = explode(',', $urlFields);
             foreach($urlFields as $field) {
