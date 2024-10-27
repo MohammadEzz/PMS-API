94fb64572d815f56876e53d0ba8d16a4c0c64ad6
Mohammad Ezz <mohammad.ezzeldin.hassan@outlook.com>
2024-10-27 09:36:46 +0400

    Refactor CityController and add CityRepository for it

--- app/Http/Controllers/Api/V1/CityController.php
+++ app/Http/Controllers/Api/V1/CityController.php
@@ -7,6 +7,7 @@ use App\Http\Helpers\Api\ApiField;
 use App\Http\Helpers\Api\ApiFilter;
 use App\Http\Helpers\Api\ApiMessagesTemplate;
 use App\Http\Helpers\Api\ApiSort;
+use App\Http\Repository\CityRepository;
 use App\Models\City;
 use Exception;
 use Illuminate\Http\Request;
@@ -18,57 +19,16 @@ class CityController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request, ApiSort $sort, ApiField $field, ApiFilter $filter)
+    public function index(Request $request, CityRepository $repository)
     {
-            $fields = [
-                "id" => "id",
-                "name" => "name",
-                "areacode" => "areacode",
-            ];
-            
-            $fieldParams = $fields;
-            $filterParams = '';
-            $sortParams = [];
-            $rangeParams = 20;
+        $fields = [
+            "id" => "id",
+            "name" => "name",
+            "areacode" => "areacode",
+        ];
+        
+        $cityItems = $repository->fetchListOfItems($request, $fields);
 
-            if($request->has('fields')) {
-                $urlFields = $request->query('fields');
-                $fieldParams = $field->buildFields($urlFields, $fields);
-            }
-
-            if($request->has('filter')) {
-                $urlFilter = $request->query('filter');
-                [$filterParams, $queryParams] = $filter->buildFilter($urlFilter, $fields);
-            }
-            
-            if($request->has('sort')) {
-                $urlSort = $request->query('sort');
-                $sortParams = $sort->buidlSort($urlSort, $fields);
-            }
-
-            if($request->has('range')) {
-                $urlRange = $request->query('range');
-                $rangeParams = strtolower($urlRange);
-            }
-            
-            // Select
-            $query = City::select($fieldParams);
-
-            // Where
-            $filterParams && $query->WhereRaw($filterParams, $queryParams);
-
-            // Order By | Sorting
-            if(count($sortParams) > 0) {
-                foreach($sortParams as $value){
-                    [$field, $sortType] = explode('.', $value);
-                    $query->orderBy($field, $sortType);
-                }
-            }
-            else
-                $query->orderBy('name', 'asc');            
-           
-            $options = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
-
-            return ApiMessagesTemplate::createResponse(true, 200, "Cities readed successfully", $options);
+        return ApiMessagesTemplate::createResponse(true, 200, "Cities readed successfully", $cityItems);
     }
 }
---
+++ app/Http/Repository/CityRepository.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Http\Repository;
+
+use App\Http\Helpers\Api\ApiField;
+use App\Http\Helpers\Api\ApiFilter;
+use App\Http\Helpers\Api\ApiSort;
+use App\Models\City;
+use Illuminate\Http\Request;
+
+class CityRepository implements GeneralRepository {
+
+    private ApiField $field;
+    private ApiFilter $filter;
+    private ApiSort $sort;
+
+    public function __construct(ApiField $field, ApiFilter $filter, ApiSort $sort)
+    {
+        $this->field = $field;
+        $this->filter = $filter;
+        $this->sort = $sort;
+    }
+
+    public function fetchListOfItems(Request $request, array $fields): array
+    {
+        $fieldParams = $fields;
+        $filterParams = '';
+        $sortParams = [];
+        $rangeParams = 'all';
+
+        if($request->has('fields')) {
+            $urlFields = $request->query('fields');
+            $fieldParams = $this->field->buildFields($urlFields, $fields);
+        }
+        else
+            $fieldParams = $this->field->aliasFieldsName($fields);
+
+        if($request->has('filter')) {
+            $urlFilter = $request->query('filter');
+            [$filterParams, $queryParams] = $this->filter->buildFilter($urlFilter, $fields);
+        }
+        
+        if($request->has('sort')) {
+            $urlSort = $request->query('sort');
+            $sortParams = $this->sort->buidlSort($urlSort, $fields);
+        }
+
+        if($request->has('range')) {
+            $urlRange = $request->query('range');
+            $rangeParams = strtolower($urlRange);
+        }
+
+        // Select
+        $query = City::select($fieldParams);
+
+        // Where
+        $filterParams && $query->WhereRaw($filterParams, $queryParams);
+
+        // Order By | Sorting
+        if(count($sortParams) > 0) {
+            foreach($sortParams as $value){
+                [$field, $sortType] = explode('.', $value);
+                $query->orderBy($field, $sortType);
+            }
+        }
+        else
+            $query->orderBy('name', 'asc');            
+       
+        $cities = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
+
+        return $cities->toArray();
+    }
+}
+
+?>
