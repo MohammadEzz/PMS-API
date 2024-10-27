6cbd8b70cc9bad990e80c515a46b9dde95f52e8c
Mohammad Ezz <mohammad.ezzeldin.hassan@outlook.com>
2024-10-27 10:30:18 +0400

    Refactore InventoryController and add InventoryRepository for it

--- app/Http/Controllers/Api/V1/InventoryController.php
+++ app/Http/Controllers/Api/V1/InventoryController.php
@@ -7,6 +7,7 @@ use App\Http\Helpers\Api\ApiFilter;
 
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\Api\ApiMessagesTemplate;
+use App\Http\Repository\InventoryRepository;
 use App\Models\Inventory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,12 +19,12 @@ class InventoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request, ApiSort $sort, ApiField $field, ApiFilter $filter)
+    public function index(Request $request, InventoryRepository $repository)
     {
         $fields = [
             "id" => "inventory.id",
             'purchaseitem_id'=>"purchaseitem_id",
-            "drug_id" => "drugs.id as drug_id",
+            "drug_id" => "drugs.id",
             "drugname" => "drugs.name",
             "drugbarcode" => "drugs.barcode",
             "quantity" => "inventory.quantity",
@@ -33,54 +34,12 @@ class InventoryController extends Controller
             "expiredate" => "inventory.expiredate",
         ];
 
-        $fieldParams = $fields;
-        $filterParams = '';
-        $sortParams = [];
-        $rangeParams = 20;
-
-        if($request->has('fields')) {
-            $urlFields = $request->query('fields');
-            $fieldParams = $field->buildFields($urlFields, $fields);
-        }
-
-        if($request->has('filter')) {
-            $urlFilter = $request->query('filter');
-            [$filterParams, $queryParams] = $filter->buildFilter($urlFilter, $fields);
-        }
-        
-        if($request->has('sort')) {
-            $urlSort = $request->query('sort');
-            $sortParams = $sort->buidlSort($urlSort, $fields);
-        }
-
-        if($request->has('range')) {
-            $urlRange = $request->query('range');
-            $rangeParams = strtolower($urlRange);
-        }
-
-        // Select & Joins
-        $query = Inventory::query()->select($fieldParams)
-        ->join('drugs', 'inventory.drug_id', '=', 'drugs.id')
-        ->join('purchaseitems', 'purchaseitems.id', '=', 'inventory.purchaseitem_id');
-
-        // Where
-        $filterParams && $query->WhereRaw($filterParams, $queryParams);
-
-        // Order By | Sort
-        if(count($sortParams) > 0) {
-            foreach($sortParams as $value){
-                [$field, $sortType] = explode('.', $value);
-                $query->orderBy($field, $sortType);
-            }
-        }
-        else $query->orderBy('id', 'desc');
-
-        $inventoryItems = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
+        $inventoryItems = $repository->fetchListOfItems($request, $fields);
         
         return ApiMessagesTemplate::createResponse(true, 200, "Purchase items readed successfully", $inventoryItems);
     }
 
-    public function quantity(Request $request, ApiSort $sort, ApiField $field, ApiFilter $filter) {
+    public function quantity(Request $request,  InventoryRepository $repository) {
         $fields = [
             'drug_id'=>"drug_id",
             "name" => "name",
@@ -88,55 +47,10 @@ class InventoryController extends Controller
             "middleunitnum" => "middleunitnum",
             "smallunitnum" => "smallunitnum",
         ];
-
-        $fieldParams = $fields;
-        $filterParams = '';
-        $sortParams = [];
-        $rangeParams = 20;
-
-        if($request->has('fields')) {
-            $urlFields = $request->query('fields');
-            $fieldParams = $field->buildFields($urlFields, $fields);
-        }
-
-        if($request->has('filter')) {
-            $urlFilter = $request->query('filter');
-            [$filterParams, $queryParams] = $filter->buildFilter($urlFilter, $fields);
-        }
         
-        if($request->has('sort')) {
-            $urlSort = $request->query('sort');
-            $sortParams = $sort->buidlSort($urlSort, $fields);
-        }
+        $quantityItems = $repository->fetchQuantity($request, $fields);
 
-        if($request->has('range')) {
-            $urlRange = $request->query('range');
-            $rangeParams = strtolower($urlRange);
-        }
-
-        // Select & Join
-        $query = Inventory::query()
-        ->select([...$fieldParams, DB::raw('sum(quantity) as quantity')])
-        ->join('drugs', 'drugs.id', '=', 'drug_id');
-
-        // Where
-        $filterParams && $query->WhereRaw($filterParams, $queryParams);
-
-        // Group By
-        $query->groupBy('drug_id');
-
-        // Order By | Sort
-        if(count($sortParams) > 0) {
-            foreach($sortParams as $value){
-                [$field, $sortType] = explode('.', $value);
-                $query->orderBy($field, $sortType);
-            }
-        }
-        else $query->orderBy('name', 'asc');
-        
-        $inventoryItems = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
-
-        return ApiMessagesTemplate::createResponse(true, 200, "Purchase items readed successfully", $inventoryItems);
+        return ApiMessagesTemplate::createResponse(true, 200, "Purchase items readed successfully", $quantityItems);
        
     }
 }
---
+++ app/Http/Repository/InventoryRepository.php
@@ -0,0 +1,130 @@
+<?php
+namespace App\Http\Repository;
+
+use App\Http\Helpers\Api\ApiField;
+use App\Http\Helpers\Api\ApiFilter;
+use App\Http\Helpers\Api\ApiSort;
+use App\Models\Inventory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class InventoryRepository implements GeneralRepository {
+
+    private ApiField $field;
+    private ApiFilter $filter;
+    private ApiSort $sort;
+
+    public function __construct(ApiField $field, ApiFilter $filter, ApiSort $sort)
+    {
+        $this->field = $field;
+        $this->filter = $filter;
+        $this->sort = $sort;
+    }
+
+    public function fetchListOfItems(Request $request, array $fields): array
+    {
+        
+        $fieldParams = $fields;
+        $filterParams = '';
+        $sortParams = [];
+        $rangeParams = 'all';
+
+        if($request->has('fields')) {
+            $urlFields = $request->query('fields');
+            $fieldParams = $this->field->buildFields($urlFields, $fields);
+        }
+        else
+            $fieldParams = $this->field->aliasFieldsName($fields);
+
+        if($request->has('filter')) {
+            $urlFilter = $request->query('filter');
+            [$filterParams, $queryParams] = $this->filter->buildFilter($urlFilter, $fields);
+        }
+        
+        if($request->has('sort')) {
+            $urlSort = $request->query('sort');
+            $sortParams = $this->sort->buidlSort($urlSort, $fields);
+        }
+
+        if($request->has('range')) {
+            $urlRange = $request->query('range');
+            $rangeParams = strtolower($urlRange);
+        }
+
+        // Select & Joins
+        $query = Inventory::query()->select($fieldParams)
+        ->join('drugs', 'inventory.drug_id', '=', 'drugs.id')
+        ->join('purchaseitems', 'purchaseitems.id', '=', 'inventory.purchaseitem_id');
+
+        // Where
+        $filterParams && $query->WhereRaw($filterParams, $queryParams);
+
+        // Order By | Sort
+        if(count($sortParams) > 0) {
+            foreach($sortParams as $value){
+                [$field, $sortType] = explode('.', $value);
+                $query->orderBy($field, $sortType);
+            }
+        }
+        else $query->orderBy('id', 'desc');
+
+        $inventoryItems = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
+
+        return $inventoryItems->toArray();
+    }
+
+    public function fetchQuantity(Request $request, array $fields) {
+        $fieldParams = $fields;
+        $filterParams = '';
+        $sortParams = [];
+        $rangeParams = 'all';
+
+        if($request->has('fields')) {
+            $urlFields = $request->query('fields');
+            $fieldParams = $this->field->buildFields($urlFields, $fields);
+        }
+        else
+            $fieldParams = $this->field->aliasFieldsName($fields);
+
+        if($request->has('filter')) {
+            $urlFilter = $request->query('filter');
+            [$filterParams, $queryParams] = $this->filter->buildFilter($urlFilter, $fields);
+        }
+        
+        if($request->has('sort')) {
+            $urlSort = $request->query('sort');
+            $sortParams = $this->sort->buidlSort($urlSort, $fields);
+        }
+
+        if($request->has('range')) {
+            $urlRange = $request->query('range');
+            $rangeParams = strtolower($urlRange);
+        }
+
+        // Select & Join
+        $query = Inventory::query()
+        ->select([...$fieldParams, DB::raw('sum(quantity) as quantity')])
+        ->join('drugs', 'drugs.id', '=', 'drug_id');
+
+        // Where
+        $filterParams && $query->WhereRaw($filterParams, $queryParams);
+
+        // Group By
+        $query->groupBy('drug_id');
+
+        // Order By | Sort
+        if(count($sortParams) > 0) {
+            foreach($sortParams as $value){
+                [$field, $sortType] = explode('.', $value);
+                $query->orderBy($field, $sortType);
+            }
+        }
+        else $query->orderBy('name', 'asc');
+
+        $quantityItems = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
+
+        return $quantityItems->toArray();
+    }
+}
+
+?>
