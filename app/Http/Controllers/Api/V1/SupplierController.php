bdeeca7df980c17f917358f58822e770c7307283
Mohammad Ezz <mohammad.ezzeldin.hassan@outlook.com>
2024-11-12 23:26:40 +0400

    Add SupplierRepository and update SupplierController

--- app/Http/Controllers/Api/V1/SupplierController.php
+++ app/Http/Controllers/Api/V1/SupplierController.php
@@ -7,61 +7,21 @@ use App\Http\Helpers\Api\ApiField;
 use App\Http\Helpers\Api\ApiFilter;
 use App\Http\Helpers\Api\ApiMessagesTemplate;
 use App\Http\Helpers\Api\ApiSort;
+use App\Http\Repository\SupplierRepository;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    public function index(Request $request, ApiSort $sort, ApiField $field, ApiFilter $filter)
+    public function index(Request $request, SupplierRepository $repository)
     {
             $fields = [
                 "id" => "id",
                 "name" => "name",
             ];
 
-            $fieldParams = $fields;
-            $filterParams = '';
-            $sortParams = [];
-            $rangeParams = 20;
+            $supplierItems = $repository->fetchListOfItems($request, $fields);
 
-            if($request->has('fields')) {
-                $urlFields = $request->query('fields');
-                $fieldParams = $field->buildFields($urlFields, $fields);
-            }
-
-            if($request->has('filter')) {
-                $urlFilter = $request->query('filter');
-                [$filterParams, $queryParams] = $filter->buildFilter($urlFilter, $fields);
-            }
-            
-            if($request->has('sort')) {
-                $urlSort = $request->query('sort');
-                $sortParams = $sort->buidlSort($urlSort, $fields);
-            }
-
-            if($request->has('range')) {
-                $urlRange = $request->query('range');
-                $rangeParams = strtolower($urlRange);
-            }
-
-            // Select
-            $query = Supplier::select($fieldParams);
-
-            // Where
-            $filterParams && $query->WhereRaw($filterParams, $queryParams);
-
-            // Order By | Sorting
-            if(count($sortParams) > 0) {
-                foreach($sortParams as $value){
-                    [$field, $sortType] = explode('.', $value);
-                    $query->orderBy($field, $sortType);
-                }
-            }
-            else $query->orderBy('name', 'asc');
-
-           
-            $options = $rangeParams == 'all' ? $query->get() : $query->paginate($rangeParams);
-
-            return ApiMessagesTemplate::createResponse(true, 200, "Supplier Readed Successfully", $options);
+            return ApiMessagesTemplate::createResponse(true, 200, "Supplier Readed Successfully", $supplierItems);
     }
 }
